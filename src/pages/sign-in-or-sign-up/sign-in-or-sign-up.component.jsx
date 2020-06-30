6d7b8b95b4bdedab26715d6b1e66281c24632d87
Akshay <akshay.radhakrishna@gmail.com>
2020-06-30 22:28:22 +0530

    Implemented firebase utils, implemented ability authenticate user

--- src/pages/sign-in-or-sign-up/sign-in-or-sign-up.component.jsx
+++ src/pages/sign-in-or-sign-up/sign-in-or-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+import './sign-in-or-sign-up.styles.scss';
 
 const SignInOrSignUp = () => {
     return (
-        <div>
+        <div className='sign-in-or-sign-up'>
             <SignIn/> 
+            <SignUp/>
         </div>
     )
 }
