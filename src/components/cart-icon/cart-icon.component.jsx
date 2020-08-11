5d48b5ef7e4068631fb37f0a6c81d5ac811f199b
Akshay <akshay.radhakrishna@gmail.com>
2020-08-11 17:07:18 +0530

    Updating the application with reselectors to handle cart items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,13 +3,15 @@ import {connect} from 'react-redux';
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {cartToggleAction} from '../../redux/cart/cart.action';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCart}) => {
+const CartIcon = ({toggleCart, cartQuantity}) => {
     return(
         <div className='cart-icon' onClick={toggleCart}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartQuantity}</span>
         </div>
     )
 }
@@ -18,4 +20,10 @@ const mapDispatchToProps = dispatch => ({
     toggleCart: () => dispatch(cartToggleAction())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => (
+    {
+        cartQuantity: selectCartItemsCount(state)
+    }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
